2801066394abfdbff0f6be59edc9a73d0763c7f9
Snyder Guerrero <jsguerre@bancolombia.com.co>
2021-11-05 15:36:49 -0500

    se agrega proyecto demo para consumo api

---
+++ src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -0,0 +1,52 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.UsuarioModel;
+import com.example.demo.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping()
+    public ArrayList<UsuarioModel> obtenerUsuarios(){
+        return usuarioService.obtenerUsuarios();
+    }
+
+    @PostMapping()
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
+        return  this.usuarioService.guardarUsuario(usuario);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<UsuarioModel> obtenerUsuarioPorId(@PathVariable("id") Long id){
+        return this.usuarioService.obtenerPorId(id);
+    }
+
+    @GetMapping("/query")
+    public ArrayList<UsuarioModel> obtenerUsuarioPorPrioridad(@RequestParam("prioridad") Integer prioridad){
+        return this.usuarioService.obtenerPorPriodidad(prioridad);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String eliminarPorId(@PathVariable("id") Long id){
+        boolean ok =  this.usuarioService.eliminarUsuario(id);
+        if(ok){
+            return "Se eliminó el usuario con el id: " + id;
+        }
+        else{
+            return  "No se pudo eliminar el usuario con el id: "+ id;
+        }
+    }
+
+}
